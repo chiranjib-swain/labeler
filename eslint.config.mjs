6f2d8de64a434ed8e7142c0de4e3084bf9a23538
chiranjib-swain <chiranjib-swain@github.com>
2026-06-25 11:55:24 +0530

    feat: add eslint-plugin-n and update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import prettierConfig from 'eslint-config-prettier';
 import jestPlugin from 'eslint-plugin-jest';
+import nPlugin from 'eslint-plugin-n';
 
 export default [
   {
@@ -27,10 +28,12 @@ export default [
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
-      jest: jestPlugin
+      jest: jestPlugin,
+      n: nPlugin
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
+      'n/no-extraneous-import': 'error',
       '@typescript-eslint/no-require-imports': 'error',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
